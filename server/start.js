1ce729eeed2808f5ddf3e90ea33d7b64655d3eef
Neville Neville <nchallinger@gmail.com>
2017-01-09 12:03:54 +1100

    Adjust ports for deployment

--- server/start.js
+++ server/start.js
@@ -5,15 +5,17 @@ const serveStatic = require('serve-static')
 
 let staticFiles = serveStatic('./client')
 
-const port = 8000
+const port = process.env.PORT || 8000
 
 // The collaborative editing document server.
-createServer((req, resp) => {
+server = createServer((req, resp) => {
   console.log("req received for:", req.url)
   if (!handleCollabRequest(req, resp)) {
     console.log('serving static files, req.url', req.url)
     staticFiles(req, resp, finalhandler(req, resp))
   }
-}).listen(port, "127.0.0.1")
+})
 
-console.log("Collab demo server listening on " + port)
+server.listen(port)
+
+console.log("Collab server listening on " + server.address().port)
