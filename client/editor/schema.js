4e362fab33c97732d0e449922e12b02162d108d8
Joel <joelbmolina@gmail.com>
2017-01-11 20:28:11 -0700

    deleted files from prosemirror

--- client/editor/schema.js
+++
@@ -1,8 +0,0 @@
-import {Schema} from 'prosemirror-model'
-import {addListNodes} from 'prosemirror-schema-list'
-const {schema: base} = require('prosemirror-schema-basic')
-
-exports.schema = new Schema({
-  nodes: addListNodes(base.nodeSpec, 'paragraph block*', 'block'),
-  marks: base.markSpec
-})
